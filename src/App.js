e81b0ce046b9993b7295957b2d052f861cfcf143
Filip Wiącek <filip@convert.no>
2021-05-16 21:09:56 +0200

    adds form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { Button, Form, Container, Header } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      itemName: '',
+      itemValue: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state);
+  }
+
+  render() {
+    const { itemName, itemValue } = this.state
+    return (
+      <Container fluid>
+        <Header as="h2">React Google Sheets</Header>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>Item name</label>
+            <input placeholder="Enter item name" name="itemName" value={itemName} onChange={this.handleChange} />
+          </Form.Field>
+          <Form.Field>
+            <label>Item value</label>
+            <input placeholder="Enter item value" name="itemValue" value={itemValue} onChange={this.handleChange} />
+          </Form.Field>
+
+          <Button color='blue' type='submit'>Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
 }
-
-export default App;
